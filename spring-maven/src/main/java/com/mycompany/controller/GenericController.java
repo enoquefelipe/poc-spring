eccc86b68895112d5faeefab3ff7f0e5c6ac0511
enoqueleal <enoque.leal01@etec.sp.gov.br>
2016-11-30 23:33:06 -0200

    Alterei o requestMapping para "/"

--- spring-maven/src/main/java/com/mycompany/controller/GenericController.java
+++ spring-maven/src/main/java/com/mycompany/controller/GenericController.java
@@ -2,11 +2,13 @@ package com.mycompany.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class GenericController {
 
-	@RequestMapping("/index")
+	@RequestMapping(method = RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
